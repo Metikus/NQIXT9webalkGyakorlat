404d2c5d0e00bff4f1bcdbfd14588fea2c60f3d8
NQIXT9 <sz.peti8991@gmail.com>
2020-10-07 19:43:22 +0200

    v1.6

--- 0917Calculator/src/Calculator/Alap.java
+++ 0917Calculator/src/Calculator/Alap.java
@@ -4,7 +4,7 @@ public class Alap {
 
 	public static void main(String[] args) {
 		int a=8, b=12, c=54, eredmeny;
-		eredmeny = a + b + c;
+		eredmeny =a + a + b + c;
 		System.out.println(eredmeny);
 	}
 
