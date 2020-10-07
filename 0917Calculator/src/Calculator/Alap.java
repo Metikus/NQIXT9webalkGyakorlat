38d0e2c2fb5b8b8e221e2432895f22c780d5bae8
NQIXT9 <sz.peti8991@gmail.com>
2020-10-07 19:38:25 +0200

    v1.4

--- 0917Calculator/src/Calculator/Alap.java
+++ 0917Calculator/src/Calculator/Alap.java
@@ -3,15 +3,9 @@ package Calculator;
 public class Alap {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		int a=8, b=12, c=54, osszeg;
-		osszeg = a + b;
-		System.out.println(osszeg);
-=======
-		int a=8, b=12, c=54,kivonas;
-		kivonas = c - a;
-		System.out.println(kivonas);
->>>>>>> 39447f140fbdf1f02657c4d9ef8e4d4f868b74e6
+		int a=8, b=12, c=54, eredmeny;
+		eredmeny = a * b;
+		System.out.println(eredmeny);
 	}
 
 }
