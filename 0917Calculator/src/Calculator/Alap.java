39447f140fbdf1f02657c4d9ef8e4d4f868b74e6
NQIXT9 <sz.peti8991@gmail.com>
2020-10-07 17:43:35 +0200

    v1.2b

--- 0917Calculator/src/Calculator/Alap.java
+++ 0917Calculator/src/Calculator/Alap.java
@@ -3,8 +3,9 @@ package Calculator;
 public class Alap {
 
 	public static void main(String[] args) {
-		int a=8, b=12, c=54;
-		System.out.println(c-a-b);
+		int a=8, b=12, c=54,kivonas;
+		kivonas = c - a;
+		System.out.println(kivonas);
 	}
 
 }
