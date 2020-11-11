d1e7c1138db255ae56ea426ebe5dac33977f2ad4
NQIXT9 <sz.peti8991@gmail.com>
2020-11-12 00:01:20 +0100

    1 and 2

---
+++ NQIXT9_1105/Calculator/src/Calculator/Alap.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+public class Alap {
+
+	public static void main(String[] args) {
+		int a=8, b=12, c=54, eredmeny;
+		eredmeny = a + b + c;
+		eredmeny = c - a - b;
+		eredmeny = a * b;
+		eredmeny = a / b;
+		System.out.println(eredmeny);
+	}
+
+}
