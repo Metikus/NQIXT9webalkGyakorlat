63060fc08a7bba360c3218d970d470f2463e5c3c
NQIXT9 <sz.peti8991@gmail.com>
2020-11-12 00:05:32 +0100

    1 and 2

---
+++ NQIXT9_1105/nqixt9springbootweb/src/main/java/hu/nqixt9/springbootweb/Nqixt9springbootwebApplication.java
@@ -0,0 +1,13 @@
+package hu.nqixt9.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nqixt9springbootwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nqixt9springbootwebApplication.class, args);
+	}
+
+}
---
+++ NQIXT9_1105/nqixt9springbootweb/src/test/java/hu/nqixt9/springbootweb/Nqixt9springbootwebApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nqixt9.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nqixt9springbootwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
